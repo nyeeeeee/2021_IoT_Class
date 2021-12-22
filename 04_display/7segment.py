d44b0ab308957f36b60963132c04913b34500a18
nyeeeeee <96505807+nyeeeeee@users.noreply.github.com>
2021-12-22 10:17:39 +0900

    Add files via upload

---
+++ 04_display/7segment.py
@@ -0,0 +1,32 @@
+#7segment.py
+import PRi.GPIO as GPIO
+import time
+
+#GPIO 7개 핀 번호 설정
+#               A  B  C  D  E  F  G
+SEGMENT_PINs = [2, 3, 4, 5, 6, 7, 8 ]
+
+GPIO.setmode(GPIO.BCM)
+
+for segment in SEDGMENT_PINS:
+    GPIO.setup(segment, GPIO.OUT)
+    GPIO.output(segment, GPIO.LOW)
+
+#Common Cathode (HIGH->ON, LOW ->OFF)
+data = [1, 1, 1, 1, 1, 1, 0]
+
+try:
+    for _ in range(3): #0~2
+        for i in range(7): #0~6
+            GPIO.output(SEGMENT_PINS[i], data[i])
+
+        time.sleep(1)
+
+        for i in range(7):
+            GPIO.output(SEGMANT_PINS[i], GPIO.LOW)
+
+        time.sleep(1)
+finally:
+    GPIO.cleanup()
+    print('bye')
+    
